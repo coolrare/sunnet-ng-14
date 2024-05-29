52ed1f6937e80b1e93d3b69fe67e1681d959e141
Will <doggy.huang@gmail.com>
2024-05-29 14:56:30 +0800

    示範雙向繫結

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  keyword = 'Angular 14'
+
   sitename = 'Hello World'
 
   changeTitle(newTitle: string) {
     this.sitename = newTitle
+  }
+
+  clearKeyword() {
+    this.keyword = ''
   }
 
 }
