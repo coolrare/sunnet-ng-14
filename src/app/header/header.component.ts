316a226d4ab9c9ef1704642fa21b56c2b1ef94c5
Will <doggy.huang@gmail.com>
2024-06-03 11:52:59 +0800

    示範 ngOnInit 與 ngOnChanges 的執行順序

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnChanges {
   @Input()
   sitename = 'Hello World';
 
@@ -15,9 +14,19 @@ export class HeaderComponent implements OnInit {
 
   counter = 0;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor', this.sitename);
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('onchange', changes['sitename']);
+  }
+
+  // 一個 callback 方法，在預設的變更檢測器首次檢查 Directive 的資料繫結屬性之後立即執行，
+  // 並且在檢查任何 View 或 Content Child 之前。它只在 Directive 實例化時執行一次。
+  ngOnInit() {
+    this.sitename = 'Hello World';
+    console.log('oninit', this.sitename);
   }
 
   changeTitle(newTitle: string) {
@@ -25,5 +34,4 @@ export class HeaderComponent implements OnInit {
 
     this.counter++;
   }
-
 }
